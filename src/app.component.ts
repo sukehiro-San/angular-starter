ba25679c4c53b51fffbe806a70839e951f0c0c33
sukehiro-San <sunnykewat99@gmail.com>
2024-08-27 12:53:02 +0530

    test-instance-created

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,9 @@
-import {}
+import { Component } from "@angular/core";
 
 @Component({
-
+    selector:"app-root",
+    template:`
+    <app-test></app-test>
+  `
 })
+export class AppComponent{}
---
+++ src/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-test",
+    template: `
+        Test Component Works!
+    `
+})
+export class TestComponent { }
